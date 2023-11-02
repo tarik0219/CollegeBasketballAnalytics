0839e40207dd8e5b5a27617e8f8abe1dba03a2f8
tarik0219 <53493085+tarik0219@users.noreply.github.com>
2023-11-02 16:02:08 -0500

    dbFileName in same directory

--- utils/db.py
+++ utils/db.py
@@ -1,8 +1,10 @@
 from tinydb import TinyDB, Query
 from tinydb.operations import set
+import os
+
+dbfile = os.path.join(os.getcwd(), "cbbweb.json")
 
 
-
-db = TinyDB('cbbweb.json')
+db = TinyDB(dbfile)
 query = Query()
 teamsTable = db.table('teams')
